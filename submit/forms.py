44dc25ec53e9904b39a1c74a27f5b35c1dbe8473
Conner <gorman.connerm@gmail.com>
2023-01-01 21:57:04 +0000

    work on css styling

--- submit/forms.py
+++ submit/forms.py
@@ -1,12 +1,13 @@
 from django import forms
-
+from django.utils.safestring import mark_safe
 
 class ListenForm(forms.Form):
     artist = forms.CharField(label="Artist:", max_length=200)
     album = forms.CharField(label="Album:", max_length=200)
     track = forms.CharField(label="Track:", max_length=200)
     duration = forms.DurationField(label='Duration (hh:mm:ss):')
-    date = forms.DateTimeField(label='Date Played (MM/DD/YY HH:MM):')
+    date = forms.DateTimeField(label='Date Played',
+                               widget=forms.TextInput(attrs={'placeholder': 'MM/DD/YY HH:MM'}))
 
 
 class ArtistForm(forms.Form):
