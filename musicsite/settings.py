0fbf0de72fa5d0786327390c09678244078531fc
conner <gorman.connerm@gmail.com>
2023-01-14 14:18:40 -0500

    clean up settings

--- musicsite/settings.py
+++ musicsite/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-&#yv*z2e2vmv$62tzg+ng*1dkx-grvo9f1u43v@))a6icjwf2y"
+SECRET_KEY = os.environ['DJANGO_SECRET']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
